2673f41784960a643c8bfa796921e4cea998d11d
dmitriy <dmitriyvasil@gmail.com>
2021-03-12 22:58:58 +0300

    init

--- api/settings.py
+++ api/settings.py
@@ -85,22 +85,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
 LANGUAGE_CODE = "ru-ru"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
