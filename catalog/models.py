c58c261250591c0492afb6cff8481699aca8d9a9
dmitriy <dmitriyvasil@gmail.com>
2021-04-05 14:43:52 +0300

    05.04.2021

--- catalog/models.py
+++ catalog/models.py
@@ -11,3 +11,31 @@ class Catalog(models.Model):
         null=True,
         to="self",
     )
+
+    @property
+    def _get_parameters(self, *args, **kwargs):
+        return {"name": self.name, "title": self.title}
+
+    @property
+    def get_child(self, *args, **kwargs):
+        if self.child.all().count() == 0:
+            blogs = list()
+            for blog in self.blog.all():
+                blogs.append({"name": blog.name, "title": blog.title})
+            return {"name": self.name, "title": self.title, "blog": blogs}
+        payload = {"name": self.name, "title": self.title, "child": list()}
+        for child in self.child.all():
+            payload["child"].append(child.get_child)
+        return payload
+
+    @property
+    def get_parent(self, *args, **kwargs):
+        if self.parent is None:
+            return [{"name": self.name, "title": self.title}]
+        payload = [
+            {
+                "name": self.name,
+                "title": self.title,
+            }
+        ] + self.parent.get_parent
+        return payload
